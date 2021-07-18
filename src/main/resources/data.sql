a5ea386f7d03b8a76e023c64ab488605ec6f68d4
Franklin Barreto <franklin-barreto@hotmail.com>
2021-07-18 16:27:52 -0300

    inserção de dados iniciais

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,44 @@
 insert into cliente(nome,cpf,data_nascimento) values('Alberto Augusto','12365478978','1984-05-15'); 
+insert into cliente(nome,cpf,data_nascimento) values('Alberto Roberto','87455447747','1988-04-20'); 
+insert into cliente(nome,cpf,data_nascimento) values('Maria Clara','74587425856','2000-09-01'); 
+insert into cliente(nome,cpf,data_nascimento) values('Juliana Santos','32589674125','1999-02-25'); 
 insert into categoria values (null, 'videogames');
-insert into categoria values (null, 'celulares');
 insert into produto (id, categoria_id, nome, preco) values (null, 1, 'Playstation 4', 2500.0);
+insert into produto (id, categoria_id, nome, preco) values (null, 1, 'X-box', 2000.0);
+insert into categoria values (null, 'celulares');
 insert into produto (id, categoria_id, nome, preco) values (null, 2, 'Samsung S21', 5000.0);
 insert into produto (id, categoria_id, nome, preco) values (null, 2, 'Iphone X', 4000.0);
+insert into produto (id, categoria_id, nome, preco) values (null, 2, 'Motorola Edge', 3800.0);
+insert into produto (id, categoria_id, nome, preco) values (null, 2, 'Xiaomi M9', 3500.0);
+insert into categoria values (null, 'roupas');
+insert into produto (id, categoria_id, nome, preco) values (null, 3, 'Jaqueta de Couro', 500.0);
+insert into produto (id, categoria_id, nome, preco) values (null, 3, 'Blusca de frio North Face', 400.0);
+insert into produto (id, categoria_id, nome, preco) values (null, 3, 'Calça Jeans Khelf', 200.0);
+insert into categoria values (null, 'eletrodomésticos');
+insert into produto (id, categoria_id, nome, preco) values (null, 4, 'Tv Philips 32 p', 2000.0);
+insert into produto (id, categoria_id, nome, preco) values (null, 4, 'Lava e seca Brastemp', 3450.0);
+insert into produto (id, categoria_id, nome, preco) values (null, 4, 'Microondas Philco', 850.0);
+
+insert into pedido VALUES (null,'2020-05-10',2500.0,1);
+insert into item_pedido VALUES (null,1,2500.0,1,1);
+
+insert into pedido VALUES (null,'2020-05-20',700.0,1);
+insert into item_pedido VALUES (null,1,200.0,2,9);
+insert into item_pedido VALUES (null,1,500.0,2,7);
+
+insert into pedido VALUES (null,'2020-05-08',1100.0,2);
+insert into item_pedido VALUES (null,1,200.0,3,9);
+insert into item_pedido VALUES (null,1,400.0,3,8);
+insert into item_pedido VALUES (null,1,500.0,3,7);
+
+insert into pedido VALUES (null,'2020-05-20',3450.0,3);
+insert into item_pedido VALUES (null,1,3450.0,4,11);
+
+insert into pedido VALUES (null,'2020-08-12',2000.0,4);
+insert into item_pedido VALUES (null,1,2000.0,5,10);
+
+insert into pedido VALUES (null,'2020-02-10',2000.0,2);
+insert into item_pedido VALUES (null,1,2000.0,6,10);
+
+
+
